15519f6289d015f7e0d60e7cf2fc24281aaf2d5d
Bwolfing <wolfington98@gmail.com>
2018-02-06 22:43:47 -0800

    App component tests use typemoq

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,40 +1,59 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { FormsModule } from "@angular/forms";
+import { of } from "rxjs/observable/of";
+import { IMock, It, Mock } from "typemoq";
+
 import { AppComponent } from './app.component';
 import { EstimateCalculatorComponent } from "./estimate-calculator/estimate-calculator.component";
 import { IRideService } from "./services/ride-service.service"
+import { MockServiceProvider } from './test/MockServiceProvider';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent, EstimateCalculatorComponent
-      ],
-      imports: [
-        FormsModule
-      ],
-      providers: [
-        IRideService.MockRideServiceProvider()
-      ]
-    }).compileComponents();
-  }));
-  
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+    const defaultEstimate: Estimate = {
+        RideCost: 2.34,
+        PrimeTimeRate: 0.15,
+        Surcharge: 1.16,
+        TotalCost: 3.5
+    };
+
+    let mockService: IMock<IRideService>;
+    let fixture: ComponentFixture<AppComponent>;
+    let app: AppComponent;
+
+    beforeEach(async(() => {
+        mockService = Mock.ofType<IRideService>();
+        mockService.setup(s => s.getEstimate(It.isAny(), It.isAny()))
+        .returns(() => of(defaultEstimate))
+
+        TestBed.configureTestingModule({
+            declarations: [
+                AppComponent, EstimateCalculatorComponent
+            ],
+            imports: [
+                FormsModule
+            ],
+            providers: [
+                MockServiceProvider.provide(IRideService).usingMock(mockService)
+            ]
+        })
+        .compileComponents()
+        .then(() => {
+            fixture = TestBed.createComponent(AppComponent);
+            app = fixture.debugElement.componentInstance;
+        });
+    }));
+
+    it('should create the app', async(() => {
+        expect(app).toBeTruthy();
+    }));
+
+    it(`should have as title 'app'`, async(() => {
+        expect(app.title).toEqual('app');
+    }));
+
+    it('should render title in a h1 tag', async(() => {
+        fixture.detectChanges();
+        const compiled = fixture.debugElement.nativeElement;
+        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    }));
 });
